111ca87de26df237da34e76a4647c84d4531c602
neiljaitly7963 <neiljaitly7963@gmail.com>
2020-08-14 14:34:13 +0530

    Updates

---
+++ precache-manifest.f71dd75eb6e1d12f93e487ecb62767d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fef3d6d59e4da2bdb5226c8266310d4",
+    "url": "/fullthrottle/index.html"
+  },
+  {
+    "revision": "775a68b8b858dd328f60",
+    "url": "/fullthrottle/static/css/2.ed5f620c.chunk.css"
+  },
+  {
+    "revision": "aec7488e8c2ff3a3d1ef",
+    "url": "/fullthrottle/static/css/main.ced5e564.chunk.css"
+  },
+  {
+    "revision": "775a68b8b858dd328f60",
+    "url": "/fullthrottle/static/js/2.7e868c45.chunk.js"
+  },
+  {
+    "revision": "c3e488c1b6c2872635b113f5babd035a",
+    "url": "/fullthrottle/static/js/2.7e868c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec7488e8c2ff3a3d1ef",
+    "url": "/fullthrottle/static/js/main.e2398965.chunk.js"
+  },
+  {
+    "revision": "9dfe6b891aebf239019f",
+    "url": "/fullthrottle/static/js/runtime-main.192c7c9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/fullthrottle/static/media/SEGA.12a3db8b.woff"
+  }
+]);
